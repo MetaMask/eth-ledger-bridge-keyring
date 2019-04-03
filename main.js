3b4930c69156d53c79beb5658a64b54ca5bdb0ff
Bruno Barbieri <brunobar79@gmail.com>
2019-04-03 15:46:44 -0400

    clean up

--- main.js
+++ main.js
@@ -5,7 +5,5 @@ import LedgerBridge from './ledger-bridge'
 
 (async () => {
     const bridge = new LedgerBridge()
-    const res = await bridge.unlock('ledger-unlock-account', `44'/60'/0'`)
-    console.log('res', res)
 })()
 console.log('MetaMask < = > Ledger Bridge initialized!')
