819e2e1b30912b8398a015cc33d8d8f2981c8fdb
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-09-01 23:38:05 -0230

    Use shared ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+
+  extends: [
+    '@metamask/eslint-config',
+    '@metamask/eslint-config/config/mocha',
+    '@metamask/eslint-config/config/nodejs',
+  ],
+
+  parser: 'babel-eslint',
+
+  parserOptions: {
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      arrowFunctions: true,
+      classes: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
+
+  plugins: [
+    'json',
+    'import',
+  ],
+
+  overrides: [{
+    files: [
+      '.eslintrc.js',
+    ],
+    parserOptions: {
+      sourceType: 'script',
+    },
+  }],
+
+  ignorePatterns: ['dist'],
+}
