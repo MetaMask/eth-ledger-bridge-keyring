3f3d6b1cbaea901809a2cca8bd577a0f38b3dcb8
= <patryk.lucka@consensys.net>
2021-01-11 14:57:25 +0100

    add new ledger live bridge

--- ledger-bridge.js
+++ ledger-bridge.js
@@ -1,16 +1,20 @@
 'use strict'
 require('buffer')
 
-import TransportU2F from '@ledgerhq/hw-transport-u2f'
+import WebSocketTransport from "@ledgerhq/hw-transport-http/lib/WebSocketTransport"
 import LedgerEth from '@ledgerhq/hw-app-eth'
 import { byContractAddress } from '@ledgerhq/hw-app-eth/erc20'
 
+const BRIDGE_URL = "ws://localhost:8435"
+const TRANSPORT_CHECK_LIMIT = 10;
+const TRANSPORT_CHECK_DELAY = 1000;
+
 export default class LedgerBridge {
-    constructor () {
+    constructor() {
         this.addEventListeners()
     }
 
-    addEventListeners () {
+    addEventListeners() {
         window.addEventListener('message', async e => {
             if (e && e.data && e.data.target === 'LEDGER-IFRAME') {
                 const { action, params } = e.data
@@ -18,37 +22,70 @@ export default class LedgerBridge {
                 switch (action) {
                     case 'ledger-unlock':
                         this.unlock(replyAction, params.hdPath)
-                    break
+                        break
                     case 'ledger-sign-transaction':
                         this.signTransaction(replyAction, params.hdPath, params.tx, params.to)
-                    break
+                        break
                     case 'ledger-sign-personal-message':
                         this.signPersonalMessage(replyAction, params.hdPath, params.message)
-                    break
-                    }
+                        break
+                    case 'ledger-close-bridge':
+                        this.cleanUp(replyAction)
+                        break;
+                }
             }
         }, false)
     }
 
-    sendMessageToExtension (msg) {
+    sendMessageToExtension(msg) {
         window.parent.postMessage(msg, '*')
     }
 
-    async makeApp () {
+    delay(ms) {
+        return new Promise((success) => setTimeout(success, ms));
+    }
+
+    checkTransportLoop(i) {
+        const iterator = i ? i : 0;
+        return WebSocketTransport.check(BRIDGE_URL).catch(async () => {
+            await this.delay(TRANSPORT_CHECK_DELAY);
+            if (iterator < TRANSPORT_CHECK_LIMIT) {
+                return this.checkTransportLoop(iterator + 1);
+            } else {
+                throw new Error('Ledger transport check timeout');
+            }
+        });
+    }
+
+    async makeApp(replyAction) {
         try {
-            this.transport = await TransportU2F.create()
-            this.app = new LedgerEth(this.transport)
+            await WebSocketTransport.check(BRIDGE_URL).catch(async () => {
+                window.open('ledgerlive://bridge?appName=Ethereum')
+                await this.checkTransportLoop()
+                this.transport = await WebSocketTransport.open(BRIDGE_URL)
+                this.app = new LedgerEth(this.transport)
+            })
         } catch (e) {
             console.log('LEDGER:::CREATE APP ERROR', e)
+            this.cleanUp();
+            throw e
         }
     }
 
-    cleanUp () {
+    cleanUp(replyAction) {
         this.app = null
-        this.transport.close()
+        if (this.transport) {
+            this.transport.close()
+        }
+        if (replyAction) {
+            this.sendMessageToExtension({
+                action: replyAction,
+                success: true,
+            })
+        }
     }
 
-    async unlock (replyAction, hdPath) {
+    async unlock(replyAction, hdPath) {
         try {
             await this.makeApp()
             const res = await this.app.getAddress(hdPath, false, true)
@@ -68,8 +105,6 @@ export default class LedgerBridge {
                 payload: { error: e.toString() },
             })
 
-        } finally {
-            this.cleanUp()
         }
     }
 
@@ -95,8 +130,6 @@ export default class LedgerBridge {
                 payload: { error: e.toString() },
             })
 
-        } finally {
-            this.cleanUp()
         }
     }
 
@@ -118,44 +151,51 @@ export default class LedgerBridge {
                 payload: { error: e.toString() },
             })
 
-        } finally {
-            this.cleanUp()
         }
     }
 
     ledgerErrToMessage (err) {
         const isU2FError = (err) => !!err && !!(err).metaData
         const isStringError = (err) => typeof err === 'string'
+        const isWrongAppError = (err) => err.message && err.message.includes('6804')
+        const isLedgerLockedError = (err) => err.message && err.message.includes('OpenFailed')
         const isErrorWithId = (err) => err.hasOwnProperty('id') && err.hasOwnProperty('message')
 
         // https://developers.yubico.com/U2F/Libraries/Client_error_codes.html
         if (isU2FError(err)) {
-          // Timeout
-          if (err.metaData.code === 5) {
-            return 'LEDGER_TIMEOUT'
-          }
+            // Timeout
+            if (err.metaData.code === 5) {
+                return 'LEDGER_TIMEOUT'
+            }
 
-          return err.metaData.type
+            return err.metaData.type
+        }
+
+        if(isWrongAppError(err)) {
+            return 'LEDGER_WRONG_APP'
+        }
+        if(isLedgerLockedError(err)) {
+            return 'LEDGER_LOCKED'
         }
 
         if (isStringError(err)) {
-          // Wrong app logged into
-          if (err.includes('6804')) {
-            return 'LEDGER_WRONG_APP'
-          }
-          // Ledger locked
-          if (err.includes('6801')) {
-            return 'LEDGER_LOCKED'
-          }
+            // Wrong app logged into
+            if (err.includes('6804')) {
+                return 'LEDGER_WRONG_APP'
+            }
+            // Ledger locked
+            if (err.includes('6801')) {
+                return 'LEDGER_LOCKED'
+            }
 
-          return err
+            return err
         }
 
         if (isErrorWithId(err)) {
-          // Browser doesn't support U2F
-          if (err.message.includes('U2F not supported')) {
-            return 'U2F_NOT_SUPPORTED'
-          }
+            // Browser doesn't support U2F
+            if (err.message.includes('U2F not supported')) {
+                return 'U2F_NOT_SUPPORTED'
+            }
         }
 
         // Other
