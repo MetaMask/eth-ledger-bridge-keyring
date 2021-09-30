c4468d3677dad40ce155b1bfaa3718e670b8ff87
Dan J Miller <danjm.com@gmail.com>
2021-09-30 11:51:09 -0230

    Allow ledger connection via WebHID, when possible (#106)
    * Add support for webhid transport to ledger-bridge.js
    
    * Update build
    
    * Fix webhid permissions
    
    * Fix webhid creation
    
    * Only use webhid if u2f is not available
    
    * Update ledger-bridge.js
    
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>
    
    Co-authored-by: Mark Stacey <markjstacey@gmail.com>

--- ledger-bridge.js
+++ ledger-bridge.js
@@ -2,6 +2,7 @@
 require('buffer')
 
 import TransportU2F from '@ledgerhq/hw-transport-u2f'
+import TransportWebHID from '@ledgerhq/hw-transport-webhid'
 import LedgerEth from '@ledgerhq/hw-app-eth'
 import WebSocketTransport from '@ledgerhq/hw-transport-http/lib/WebSocketTransport'
 
@@ -84,9 +85,16 @@ export default class LedgerBridge {
                     this.app = new LedgerEth(this.transport)
                 }
             }
-            else {
+            // This is the approach used to check for u2f support in the library that the ledger transport relies on https://github.com/grantila/u2f-api/blob/master/lib/u2f-api.ts#L76
+            else if (typeof window.u2f !== 'undefined') {
                 this.transport = await TransportU2F.create()
                 this.app = new LedgerEth(this.transport)
+            }
+            else if (navigator.hid !== undefined) {
+                this.transport = await TransportWebHID.create()
+                this.app = new LedgerEth(this.transport)
+            } else {
+                throw new Error('No transport types are supported.');
             }
         } catch (e) {
             console.log('LEDGER:::CREATE APP ERROR', e)
