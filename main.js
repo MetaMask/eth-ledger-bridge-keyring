9b1a320103d3cf27ff2cf685fbbe9a3cdf1272aa
David Walsh <davidwalsh83@gmail.com>
2022-03-29 09:50:02 -0500

    Provide connect and disconnect events from the transport (#130)

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 'use strict'
 import LedgerBridge from './ledger-bridge'
 
-(async () => {
-    const bridge = new LedgerBridge()
-})()
+new LedgerBridge()
 console.log(`MetaMask < = > Ledger Bridge initialized from ${window.location}!`)
