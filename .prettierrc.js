3c0c618991f5f3b8e24e8a6b178bead87c375143
Michele Esposito <michele.esposito@consensys.net>
2023-03-20 10:19:20 +0100

    fix: use prettierrc from module template

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,8 @@
-/**
- * This file just exists to enable devs that use vscode to benefit from auto
- * formatting on save when using prettier plugin and the require config file
- * setting. It grabs the config from the shared eslint-config and re-exports
- * it to prevent any issues with mismatched settings
- */
-const config = require('@metamask/eslint-config');
-
-const prettierConfig = config.rules[`prettier/prettier`][1];
-
-module.exports = prettierConfig;
+// All of these are defaults except singleQuote, but we specify them
+// for explicitness
+module.exports = {
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
