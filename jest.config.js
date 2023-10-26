e5c1d0c7edacf24a06f8065bb4bf1b3c5e20a9b0
Jongsun Suh <34228073+MajorLift@users.noreply.github.com>
2023-10-26 13:05:31 -0700

    Adjust jest coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -41,10 +41,10 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 65.42,
+      branches: 68.06,
       functions: 88.57,
-      lines: 81.63,
-      statements: 81.55,
+      lines: 81.29,
+      statements: 81.21,
     },
   },
 
