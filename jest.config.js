5dde0ba39106d81e9d101565e3a1d131bb3dd041
stanleyyuen <102275989+stanleyyconsensys@users.noreply.github.com>
2023-11-30 14:31:55 +0800

    feat: update jest score

--- jest.config.js
+++ jest.config.js
@@ -41,10 +41,10 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 68.06,
-      functions: 88.57,
-      lines: 81.29,
-      statements: 81.21,
+      branches: 71.42,
+      functions: 89.18,
+      lines: 81.93,
+      statements: 81.84,
     },
   },
 
