7375fa082561eccecde2b4879b09ac8ca3b7d1ac
Xiaoming Wang <dawnseeker8@gmail.com>
2024-03-28 10:52:53 +0000

    feat: upgrade @ethereumjs/tx library to match the same version used in TransactionController 8.0.1 to fix all transaction broken issue.
    Fix the signPersonalMessage in ledger always return 0x0 in v value in test-dapp

--- jest.config.js
+++ jest.config.js
@@ -41,10 +41,10 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 72.78,
+      branches: 72.97,
       functions: 89.47,
-      lines: 83.71,
-      statements: 83.62,
+      lines: 83.83,
+      statements: 83.75,
     },
   },
 
